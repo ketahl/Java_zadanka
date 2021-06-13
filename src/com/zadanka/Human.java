4e39fcdedfe0d75ba279b55175a124a49df2ed27
ketahl <alanpocwiardowski.99@gmail.com>
2021-06-13 18:50:00 +0200

    Zadanie 3 i 4

--- src/com/zadanka/Human.java
+++ src/com/zadanka/Human.java
@@ -1,6 +1,31 @@
 package com.zadanka;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 public class Human {
     Animal pet;
     public Car car;
+    private Double salary;
+
+    public Double getSalary() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println();
+        System.out.println("Pobrano dane o wypłacie dnia: " + formatter.format(date) + " a wartość wypłaty to: " + salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary <= 0) {
+            System.out.println("To za pracę mam dopłacać?");
+        }
+        else
+        {
+            System.out.println("Informacje zostały wysłane do systemu księgowego");
+            System.out.println("Konieczne jest wybranie aneksu od Pani Hani z kadr");
+            System.out.println("ZUS, US i Twój agent FBI zostali poinformowani. Nie ma sensu ukrywać dochodu");
+            this.salary = salary;
+        }
+    }
 }
--- src/com/zadanka/Main.java
+++ src/com/zadanka/Main.java
@@ -3,9 +3,11 @@ package com.zadanka;
 public class Main {
 
     public static void main(String[] args) {
-		Human seba = new Human();
-		Car hondaSzybszaNizWyglada = new Car("Civic", "Honda", true, true);
-		seba.car = hondaSzybszaNizWyglada;
-		System.out.println(seba.car);
+		Human c1 = new Human();
+		c1.setSalary(100.0);
+		c1.getSalary();
+		c1.setSalary(300.0);
+		c1.getSalary();
+		c1.setSalary(-100.0);
     }
 }
